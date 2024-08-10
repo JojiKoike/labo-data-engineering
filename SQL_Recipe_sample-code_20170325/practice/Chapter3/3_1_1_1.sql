8688af8fb4ecbef41316e9a27556226dede302a2
JojiKoike <5671422+JojiKoike@users.noreply.github.com>
2024-08-10 10:13:08 +0000

    ✨ Case

---
+++ SQL_Recipe_sample-code_20170325/practice/Chapter3/3_1_1_1.sql
@@ -0,0 +1,10 @@
+SELECT
+	user_id,
+	CASE
+		WHEN register_device = 1 THEN 'PC'
+		WHEN register_device = 2 THEN 'SP'
+		WHEN register_device = 3 THEN 'App'
+		ELSE ''
+	END AS device_name
+FROM
+	mst_users;
