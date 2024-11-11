2de4bc6d52592762c2011ed3afb212c4a1277c27
JojiKoike <5671422+JojiKoike@users.noreply.github.com>
2024-11-11 15:12:04 +0000

    ✨ Bashの配列と連想配列

--- shellgei160/learn/2.1.f.sh
+++ shellgei160/learn/2.1.f.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+a=("$SHELL" "$LANG" "$USER")
+
 declare -A b
 b["SHELL"]="$SHELL"
 b["LANG"]="$LANG"
 b["USER"]="$USER"
 
 echo ${b["LANG"]}
+echo ${a[1]}
+
+IFS=:
+echo "${b[@]}"
+echo "${b[*]}"
+echo "${a[@]}"
+echo "${a[*]}"
+
+echo ${#a[@]}
+echo ${#a[*]}
+echo ${#b[@]}
+echo ${#b[*]}
+
+echo "${!b[@]}"
+echo "${!b[*]}"
